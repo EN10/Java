3dd921b802e4c45bd5751ce85e90de7be31ee1c7
Enio Fernandes <EN10@users.noreply.github.com>
2020-05-20 22:35:15 +0100

    Create FileWrite.java

---
+++ Essentials/FileWrite.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class FileWrite{
+
+    public static void main(String args[])throws IOException{
+            FileWriter out = new FileWriter("file.txt");
+            out.write("text to file");
+            out.close();
+    }
+
+}
+//https://www.tutorialspoint.com/java/java_files_io.htm
